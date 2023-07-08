09d27476321e83dc62bca320456d202e662c4564
AWahabak <128252012+AWahabak@users.noreply.github.com>
2023-07-08 18:15:45 +0100

    featureSun is added

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,10 @@ public class Main {
         Omnax.Sun="1";
         Omnax.Planets="2";
         Omnax.Stars="4";
+
+        FeatureSun Xeon = new FeatureSun();
+        Xeon.Name="tree";
+        Xeon.size="434343";
+        Xeon.temp="4545 C";
     }
 }
